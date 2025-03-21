163a52f1d32d751debd871c0f660e056a3ed4291
arfoysal <arfoysal.me@gmail.com>
2025-03-21 10:56:09 +0600

    Annotations, Assertion

--- src/test/java/org/example/DriverSetup.java
+++ src/test/java/org/example/DriverSetup.java
@@ -23,4 +23,28 @@ public class DriverSetup {
     public WebElement getElement(By locator){
         return driver.findElement(locator);
     }
+
+    public void loadPage(String url){
+        driver.get(url);
+    }
+
+    public void clickOnElement(By locator){
+        getElement(locator).click();
+    }
+
+    public void writeOnElement(By locator, String text){
+        getElement(locator).sendKeys(text);
+    }
+
+    public String getAttributeValue(By locator, String attributeName){
+        return getElement(locator).getAttribute(attributeName);
+    }
+
+    public Boolean isVisible(By locator){
+        return getElement(locator).isDisplayed();
+    }
+
+    public String getElementText(By locator){
+        return getElement(locator).getText();
+    }
 }
